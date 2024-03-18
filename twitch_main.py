d6991c3760f5e9f54513f1463763b8726c2f9a06
Nelson Diaz <Diazo0605@gmail.com>
2024-03-17 23:02:36 -0400

    reverted stuff

--- twitch_main.py
+++ twitch_main.py
@@ -89,7 +89,7 @@ class Bot(commands.Bot):
                 )
                 authorid = cur.fetchone()
                 cur.execute(
-                    f"SELECT TwitchName FROM Economy WHERE TwitchName = {message.author.name}"
+                    f"SELECT TwitchName FROM Economy WHERE TwitchName = '{message.author.name}'"
                 )
                 authorname = cur.fetchone()
 
@@ -100,13 +100,13 @@ class Bot(commands.Bot):
                     con.commit()
                 if authorid is None and authorname is not None:
                     cur.execute(
-                        f"UPDATE Economy SET TwitchID = {message.author.id} WHERE TwitchName = {message.author.name}"
+                        f"UPDATE Economy SET TwitchID = {message.author.id} WHERE TwitchName = '{message.author.name}'"
                     )
                     con.commit()
 
                 if authorid is not None and authorname is not None:
                     cur.execute(
-                        f"UPDATE Economy SET Potatoes = Potatoes + {1}, TwitchName = {message.author.name} WHERE TwitchID = {message.author.id}"
+                        f"UPDATE Economy SET Potatoes = Potatoes + {1}, TwitchName = '{message.author.name}' WHERE TwitchID = {message.author.id}"
                     )
                     con.commit()
 
